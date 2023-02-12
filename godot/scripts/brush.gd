9f97c34657f1ed865f78ba5043290fffd7f9ca52
JT Ziolo <jason.t.ziolo@gmail.com>
2023-02-11 17:40:15 -0700

    refactor: :truck: Added child viewport in scene
    Added border to the screen by moving falling sand environment into a child viewport with smaller dimensions. Also changed base resolution to match phone screens, and TODO: will adjust resize behavior to snap

--- godot/scripts/brush.gd
+++ godot/scripts/brush.gd
@@ -10,5 +10,5 @@ export(Resource) var selected_grain_type
 func _process(_delta):
 	if not Input.is_action_pressed("paint"):
 		return
-	var mouse_pos = get_viewport().get_mouse_position()
-	emit_signal("painted", [mouse_pos], selected_grain_type)
+	var mouse_position = get_viewport().get_mouse_position()
+	emit_signal("painted", [mouse_position], selected_grain_type)

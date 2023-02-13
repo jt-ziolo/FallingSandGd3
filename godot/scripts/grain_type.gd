254e85bca3dce2ea607a667f57cadc778756798d
JT Ziolo <jason.t.ziolo@gmail.com>
2023-02-13 02:24:05 -0700

    refactor: Added human readable name field to grain_type and overwrote _to_string

--- godot/scripts/grain_type.gd
+++ godot/scripts/grain_type.gd
@@ -2,4 +2,7 @@ class_name GrainType
 extends Resource
 # Holds data related to a grain's type, to be used when drawing
 
+export var human_friendly_name:String = "Example Name"
 export var color:Color = Color.tan
+func _to_string():
+	return human_friendly_name

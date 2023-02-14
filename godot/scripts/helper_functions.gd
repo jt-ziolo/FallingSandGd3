68f53267ebc4687c0af89767f795783d43236c30
JT Ziolo <jason.t.ziolo@gmail.com>
2023-02-14 16:12:28 -0700

    feat: :technologist: Added helper function for adding together two integer arrays containing two elements each

--- godot/scripts/helper_functions.gd
+++ godot/scripts/helper_functions.gd
@@ -5,3 +5,8 @@ static func nonmut_shuffle(array):
 	var copy_array = array.duplicate()  # TODO: Is deep copy needed?
 	copy_array.shuffle()
 	return copy_array
+
+
+static func add_points(point_a, point_b):
+	var result = [point_a[0] + point_b[0], point_a[1] + point_b[1]]
+	return result

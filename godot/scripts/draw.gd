13ccc11b7eb05732d49e7c9766df9fc60226f203
JT Ziolo <jason.t.ziolo@gmail.com>
2023-02-15 13:05:41 -0700

    feat: :sparkles: Draw.gd now can store a history of frames and use this information for effects such as trails or lightening/darkening.

--- godot/scripts/draw.gd
+++ godot/scripts/draw.gd
@@ -1,23 +1,33 @@
 extends Node2D
 class_name Draw
 
-var _color_coords = {}
+const MAX_HISTORY_LENGTH = 3
+
+var _color_coords_history = []
 var _was_empty_last_frame = false
 
 
 func _draw():
-	# Draw the forward layer
-	for pos in _color_coords.keys():
-		var pos_vec2 = Vector2(pos[0], pos[1])
-		draw_rect(Rect2(pos_vec2, Vector2(1, 1)), _color_coords[pos], true, 1.0, false)
+	var counter = 1.0
+	for _color_coords in _color_coords_history:
+		for pos in _color_coords.keys():
+			var color = _color_coords[pos]
+			var factor = 1.0 / (MAX_HISTORY_LENGTH as float) * counter
+			# color = color.lightened(1.0 - factor)
+			color.a = factor
+			var pos_vec2 = Vector2(pos[0], pos[1])
+			draw_primitive([pos_vec2], [color], [Vector2.ONE])
+		counter += 1.0
 
 
 func _on_Grid_colors_transmitted(colors: Dictionary):
-	if colors.empty():
+	if _color_coords_history.size() == 0:
 		if _was_empty_last_frame:
 			return
 		_was_empty_last_frame = true
 	else:
 		_was_empty_last_frame = false
-	_color_coords = colors
+	if _color_coords_history.size() == MAX_HISTORY_LENGTH:
+		_color_coords_history.pop_front()
+	_color_coords_history.append(colors)
 	update()

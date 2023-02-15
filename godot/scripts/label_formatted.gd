b798c6642b9a10bec99050e960223031f4363510
JT Ziolo <jason.t.ziolo@gmail.com>
2023-02-14 18:54:25 -0700

    feat: :children_crossing: Updated formatted label to show information including fps and a placeholder controls help subscreen. TODO: add grain count, element color and name, and hook up relevant controls

--- godot/scripts/label_formatted.gd
+++ godot/scripts/label_formatted.gd
@@ -6,9 +6,15 @@ export(String, MULTILINE) var _template_text
 var _mouse_position = [0, 0]
 
 var format_dictionary = {
-	"element_color": "maroon",
-	"element_name": "N/A",
-	"brush_mode": "N/A",
+	"element_color": "gray",
+	"element_name": "None",
+	"fps": 60,
+	"grain_count": 0,
+	"key_help": "H",
+	"key_prev": "Q",
+	"key_next": "E",
+	"key_options": "RMB",
+	"key_quit": "ESC",
 }
 
 
@@ -17,7 +23,9 @@ func _process(_delta):
 		self.bbcode_text = _template_text.format(
 			{"mouse_position": "{0}, {1}".format([_mouse_position[0], _mouse_position[1]])}
 		)
-	self.bbcode_text = _template_text.format(format_dictionary)
+	format_dictionary["fps"] = Performance.get_monitor(Performance.TIME_FPS)
+	self.bbcode_text = self.bbcode_text.format(format_dictionary)
+	self.bbcode_enabled = true
 
 
 func _on_Brush_mouse_moved(mouse_position):

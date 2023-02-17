7fb76995266aed7143c25b9157a91b0f7b5af65e
JT Ziolo <jason.t.ziolo@gmail.com>
2023-02-16 20:16:51 -0700

    fix: :bug: Brush now follows cursor again

--- godot/scripts/color_rect_mouse.gd
+++ godot/scripts/color_rect_mouse.gd
@@ -4,4 +4,4 @@ class_name ColorRectMouse
 
 # TODO: Set the color to invert the screen color under the cursor
 func _on_Brush_mouse_moved(mouse_position):
-	self.set_global_position(Vector2(mouse_position[0] as float, mouse_position[1] as float))
+	self.set_position(Vector2(mouse_position[0] as float, mouse_position[1] as float))

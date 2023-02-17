26ee6925855d4f02c8363e61ea191e7f21c61b22
JT Ziolo <jason.t.ziolo@gmail.com>
2023-02-17 16:23:52 -0700

    feat(drawing)!: add draw adjustments for colors
    Added drawing adjustments that apply based on color red and blue values to
    stylize the result.
    
    BREAKING CHANGE: elements with red and blue color values above thresholds will
    draw differently

--- godot/scripts/draw.gd
+++ godot/scripts/draw.gd
@@ -1,10 +1,6 @@
 extends Node2D
 class_name Draw
 
-# const MAX_HISTORY_LENGTH = 1
-
-# var _color_coords_history = []
-# var _was_empty_last_frame = false
 var _do_clear = true
 var _colors = {}
 
@@ -12,33 +8,28 @@ var _colors = {}
 func _draw():
 	for pos in _colors:
 		var color = _colors[pos]
+
+		# Some simple color manipulations to make things look more interesting
+		# TODO: Make recoloring a feature of the element, or use a shader pass
+		# to accomplish this
+
+		# Fire/Lava
+		if color.r > 0.7:
+			color = color.lightened(rand_range(0, 0.3))
+		# Water
+		if color.b > 0.7:
+			var rand = rand_range(0, 0.5)
+			color = color.lightened(rand*rand)
+
 		draw_primitive([pos], [color], [Vector2.ONE])
 	if _do_clear:
 		var viewport = get_viewport()
 		viewport.render_target_clear_mode = viewport.CLEAR_MODE_ONLY_NEXT_FRAME
 		_do_clear = false
-	# var counter = 1.0
-	# for _color_coords in _color_coords_history:
-	# for pos in _color_coords.keys():
-	# var color = _color_coords[pos]
-	# var factor = 1.0 / (MAX_HISTORY_LENGTH as float) * counter
-	# color = color.lightened(1.0 - factor)
-	# color.a = factor
-	# draw_primitive([pos], [color], [Vector2.ONE])
-	# counter += 1.0
 
 
 func _on_Grid_colors_transmitted(colors: Dictionary):
 	_colors = colors
-	# if _color_coords_history.size() == 0:
-	# if _was_empty_last_frame:
-	# return
-	# _was_empty_last_frame = true
-	# else:
-	# _was_empty_last_frame = false
-	# if _color_coords_history.size() == MAX_HISTORY_LENGTH:
-	# _color_coords_history.pop_front()
-	# _color_coords_history.append(colors)
 	update()
 
 func _on_Grid_colors_clear():

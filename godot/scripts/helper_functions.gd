7dbc15c59f7b3cd384eedc121929cca456bf888a
JT Ziolo <jason.t.ziolo@gmail.com>
2023-02-16 19:57:08 -0700

    fix: :bug: Adjusted scene tree hierarchy to fix issue where part of the screen was not visible.

--- godot/scripts/helper_functions.gd
+++ godot/scripts/helper_functions.gd
@@ -10,7 +10,16 @@ static func nonmut_shuffle(array):
 static func add_points(point_a, point_b):
 	var result = [point_a[0] + point_b[0], point_a[1] + point_b[1]]
 	return result
+	
 
+static func interpolate_between_points(point_a, point_b, factor):
+	var a_x = point_a[0]
+	var b_x = point_b[0]
+	var a_y = point_a[1]
+	var b_y = point_b[1]
+	var result = [(b_x - a_x) * factor + a_x, (b_y - a_y) * factor + a_y]
+	result = [result[0] as int, result[1] as int]
+	return result
 
 static func sort_ascending(point_a, point_b):
 	# Return true if b should be after a
